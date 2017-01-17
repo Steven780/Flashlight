c120796ac4df49949ffbc84867b5c9cf452de10b
Steven Kinghorn <skinghorn780@gmail.com>
2017-01-17 21:36:52 +0000

    Created a Settings.java class for new activity with settings linear layout xml

---
+++ app/src/main/java/com/androidhive/flashlight/flashlight/SettingsActivity.java
@@ -0,0 +1,13 @@
+package com.androidhive.flashlight.flashlight;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by stevenkinghorn on 17/01/2017.
+ */
+
+public class SettingsActivity extends AppCompatActivity {
+
+
+}
+

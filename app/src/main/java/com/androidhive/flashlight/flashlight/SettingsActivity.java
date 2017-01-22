63536cf660f4783dd1e80135f2908dcf8dcf23f9
Steven Kinghorn <skinghorn780@gmail.com>
2017-01-22 19:18:42 +0000

    Added two checkboxes to settings.xml with function in SettingsActivity.java to handle onClicks

--- app/src/main/java/com/androidhive/flashlight/flashlight/SettingsActivity.java
+++ app/src/main/java/com/androidhive/flashlight/flashlight/SettingsActivity.java
@@ -3,6 +3,8 @@ package com.androidhive.flashlight.flashlight;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.CheckBox;
 
 
 public class SettingsActivity extends BaseActivity {
@@ -22,6 +24,31 @@ public class SettingsActivity extends BaseActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
+
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // Check which checkbox was clicked
+        switch(view.getId()) {
+            case R.id.checkbox_meat:
+                if (checked) {
+                    // Put some meat on the sandwich
+                }else {
+                    // Remove the meat
+                    break;
+                }
+            case R.id.checkbox_cheese:
+                if (checked) {
+                    // Cheese me
+                }else {
+                    // I'm lactose intolerant
+                    break;
+                }
+            // TODO: Veggie sandwich
+        }
+    }
+
 
 }
 
